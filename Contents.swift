c36440222e72b526a4b1104509d8264c8afc1001
Afnanxt <140313979+Afnanxt@users.noreply.github.com>
2023-07-26 12:25:05 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+var score = 7
+score+=10
+
+
+// task-2
+if score>9{
+    print("pass")
+    
+}
+else {
+    
+    print("fail")
+}
+
+//task -3
+
+var names = ["afnan","may","maryam","aisha"]
+
+for name in names {
+    print(name)
+}
+
+
+// task -4
+var fruits = ["orange":7, "mango": 6 , "apple" : 2]
+
+for f in fruits {
+    print(f)
+}
+
+
+// task - 5
+let myName = "afnan"
+let msg = "hello \(myName)"
+print(msg)
+
